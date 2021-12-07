e49a8a067b8e26c15167ff1109aa93c5bb0d54c6
zhusain1 <zhusain1@umbc.edu>
2021-12-06 19:23:02 -0500

    resolve dependencies

--- src/functional_components/MyWorkouts.js
+++ src/functional_components/MyWorkouts.js
@@ -6,12 +6,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import Exercise from './Exercise';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Skeleton from '@material-ui/lab/Skeleton';
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 
 const link = {
     color: 'white',
